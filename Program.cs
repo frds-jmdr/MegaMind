6cd40f09c1fe2a16932402c59561bf7e3006e24b
Firdosh Jamadar <firdoshjamadar0@gmail.com>
2024-09-11 21:51:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+
+using MegaMind.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Users}/{action=Index}/{id?}");
+
+app.Run();
